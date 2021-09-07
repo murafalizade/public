60a77241321657ce798f4acd59740dae683f2ef1
murafalizade <hmmuralizade29@gmail.com>
2021-09-07 22:56:03 +0400

    required field

--- index.js
+++ index.js
@@ -33,38 +33,88 @@ if (window.innerWidth > 992) {
 }); 
 
 function forButton(){
-	document.getElementById('contentdis1').style.display = 'block'
+	document.getElementById('contentdis1').style.display = 'none'
 	document.getElementById('contentdis2').style.display = 'none'
-	document.getElementById('contentdis3').style.display = 'none'
+	document.getElementById('contentdis3').style.display = 'block'
 	document.getElementById('contentdis4').style.display = 'none'
 }
 
-document.getElementById('con2').addEventListener("click",function(e){
-	document.getElementById('contentdis2').style.display = 'block'
-	document.getElementById('contentdis1').style.display = 'none'
-	document.getElementById('contentdis3').style.display = 'none'
-	document.getElementById('contentdis4').style.display = 'none'
-});
-
-document.getElementById('con1').addEventListener("click", function (e) {
-	document.getElementById('contentdis1').style.display = 'block'
-	document.getElementById('contentdis2').style.display = 'none'
-	document.getElementById('contentdis3').style.display = 'none'
-	document.getElementById('contentdis4').style.display = 'none'
-});
-
-document.getElementById('con3').addEventListener("click", function (e) {
-	document.getElementById('contentdis2').style.display = 'none'
-	document.getElementById('contentdis1').style.display = 'none'
-	document.getElementById('contentdis3').style.display = 'block'
-	document.getElementById('contentdis4').style.display = 'none'
-});
-
-document.getElementById('con4').addEventListener("click", function (e) {
-	document.getElementById('contentdis2').style.display = 'none'
-	document.getElementById('contentdis1').style.display = 'none'
-	document.getElementById('contentdis3').style.display = 'none'
-	document.getElementById('contentdis4').style.display = 'block'
-});
+function step1(){
+	const inpt = document.getElementById('inputEmail4').value;
+	const inpt2 = document.getElementById('inputPassword4').value;
+	const inpt3 = document.getElementById('inputState').value;
+	const inpt4 = document.getElementById('inputAddress2').value;
+	const inpt5 = document.getElementById('inputState').value;
+	if (inpt !== "" && !inpt2 !== "" && !inpt3 !== "" && !inpt4 !== "" && !inpt4 !== "" && inpt5 !== "") {
+		console.log(inpt);
+		document.getElementById('contentdis2').style.display = 'block'
+		document.getElementById('contentdis1').style.display = 'none'
+		document.getElementById('contentdis3').style.display = 'none'
+		document.getElementById('contentdis4').style.display = 'none'
+	}
+	else{
+		alert('You have must fill all required area');
+	}
+}
 
 
+
+
+
+
+
+function step2() {
+	const inpt = document.getElementById('customFile').value.files;
+	const inpt2 = document.getElementById('exampleFormControlTextarea2').value;
+	const inpt3 = document.getElementById('exampleFormControlTextarea3').value;
+	const inpt4 = document.getElementById('exampleFormControlTextarea4').value;
+	const inpt5 = document.getElementById('customFile2').value.files;
+	if (!inpt && !inpt2 !== "" && !inpt3 !== "" && !inpt4 !== "" && !inpt4 !== "" && !inpt5) {
+		console.log(inpt);
+		document.getElementById('contentdis2').style.display = 'none'
+		document.getElementById('contentdis1').style.display = 'none'
+		document.getElementById('contentdis3').style.display = 'none'
+		document.getElementById('contentdis4').style.display = 'block'
+	}
+	else {
+		alert('You have must fill all required area');
+	}
+}
+
+
+
+
+
+
+
+
+// document.getElementById('con2').addEventListener("click",function(e){
+// 	document.getElementById('contentdis2').style.display = 'block'
+// 	document.getElementById('contentdis1').style.display = 'none'
+// 	document.getElementById('contentdis3').style.display = 'none'
+// 	document.getElementById('contentdis4').style.display = 'none'
+// });
+
+// document.getElementById('con1').addEventListener("click", function (e) {
+// 	document.getElementById('contentdis1').style.display = 'block'
+// 	document.getElementById('contentdis2').style.display = 'none'
+// 	document.getElementById('contentdis3').style.display = 'none'
+// 	document.getElementById('contentdis4').style.display = 'none'
+// });
+
+// document.getElementById('con3').addEventListener("click", function (e) {
+// 	document.getElementById('contentdis2').style.display = 'none'
+// 	document.getElementById('contentdis1').style.display = 'none'
+// 	document.getElementById('contentdis3').style.display = 'block'
+// 	document.getElementById('contentdis4').style.display = 'none'
+// });
+
+// document.getElementById('con4').addEventListener("click", function (e) {
+// 	document.getElementById('contentdis2').style.display = 'none'
+// 	document.getElementById('contentdis1').style.display = 'none'
+// 	document.getElementById('contentdis3').style.display = 'none'
+// 	document.getElementById('contentdis4').style.display = 'block'
+// });
+
+
+console.log(document.getElementsByName('input'));
